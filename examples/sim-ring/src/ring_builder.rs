4bb77e3f3579de25deac443ed17e3c7a8b1247f7
Peter Hedinger <peterh@graphcore.ai>
2025-06-10 09:53:01 +0100

    feat(examples): add examples/sim-ring as a larger simulation

---
+++ examples/sim-ring/src/ring_builder.rs
@@ -0,0 +1,148 @@
+// Copyright (c) 2025 Graphcore Ltd. All rights reserved.
+//
+//! Library functions to build parts of the SimRing application.
+
+use steam_components::arbiter::WeightedRoundRobinPolicy;
+use steam_components::flow_controls::limiter::Limiter;
+use steam_components::rc_limiter;
+use steam_components::router::Route;
+use steam_components::sink::Sink;
+use steam_components::source::Source;
+use steam_engine::engine::Engine;
+use steam_engine::traits::Routable;
+use steam_models::ethernet_frame::{EthernetFrame, u64_to_mac};
+use steam_models::fc_pipeline::FcPipeline;
+use steam_models::ring_node::{IO_INDEX, RING_INDEX, RingConfig, RingNode};
+
+use crate::packet_gen::PacketGen;
+
+// Define some types to aid readability
+pub type Limiters = Vec<Limiter<EthernetFrame>>;
+pub type Nodes = Vec<RingNode<EthernetFrame>>;
+pub type Pipes = Vec<FcPipeline<EthernetFrame>>;
+pub type Sources = Vec<Source<EthernetFrame>>;
+pub type Sinks = Vec<Sink<EthernetFrame>>;
+
+pub struct Config {
+    pub ring_size: usize,
+    pub ring_priority: usize,
+    pub rx_buffer_frames: usize,
+    pub tx_buffer_frames: usize,
+    pub packet_payload_bytes: usize,
+    pub num_send_packets: usize,
+}
+
+struct Router(usize);
+
+impl<T> Route<T> for Router
+where
+    T: Routable,
+{
+    fn route(&self, obj: &T) -> usize {
+        // If the dest matches then exit via port 1, otherwise use port 0 as that is the
+        // ring
+        let dest = obj.dest() as usize;
+        if self.0 == dest { IO_INDEX } else { RING_INDEX }
+    }
+}
+
+pub fn build_ring_nodes(engine: &mut Engine, config: &Config) -> Nodes {
+    let spawner = engine.spawner().clone();
+    let clock = engine.default_clock().clone();
+    let limiter_128_gbps = rc_limiter!(clock.clone(), 128);
+    let ring_config = RingConfig::new(config.rx_buffer_frames, config.tx_buffer_frames);
+    let top = engine.top();
+    let ring_nodes: Nodes = (0..config.ring_size)
+        .map(|i| {
+            let weights = vec![config.ring_priority, 1];
+            RingNode::new(
+                top,
+                format!("node{i}").as_str(),
+                spawner.clone(),
+                &ring_config,
+                limiter_128_gbps.clone(),
+                Box::new(Router(i)),
+                Box::new(WeightedRoundRobinPolicy::new(weights, 2)),
+            )
+        })
+        .collect();
+    ring_nodes
+}
+
+pub fn build_source_sinks(engine: &mut Engine, config: &Config) -> (Sources, Sinks) {
+    let mut sources = Vec::with_capacity(config.ring_size);
+    let top = engine.top();
+
+    for i in 0..config.ring_size {
+        let neighbour_left = if i > 0 { i - 1 } else { config.ring_size - 1 };
+
+        sources.push(Source::new(
+            top,
+            format!("source{i}").as_str(),
+            Some(Box::new(PacketGen::new(
+                top,
+                u64_to_mac(neighbour_left as u64),
+                config.packet_payload_bytes,
+                config.num_send_packets,
+            ))),
+        ));
+    }
+
+    let sinks: Sinks = (0..config.ring_size)
+        .map(|i| Sink::new(top, format!("sink{i}").as_str()))
+        .collect();
+
+    (sources, sinks)
+}
+
+pub fn build_pipes(engine: &mut Engine, config: &Config) -> (Pipes, Pipes) {
+    let mut ingress_pipes = Vec::with_capacity(config.ring_size);
+    let mut ring_pipes = Vec::with_capacity(config.ring_size);
+
+    let clock = engine.default_clock();
+    let top = engine.top();
+    let spawner = engine.spawner();
+    for i in 0..config.ring_size {
+        ingress_pipes.push(FcPipeline::new(
+            top,
+            format!("pipe{i}").as_str(),
+            clock.clone(),
+            spawner.clone(),
+            500,
+            500,
+            500,
+        ));
+        ring_pipes.push(FcPipeline::new(
+            top,
+            format!("pipe{i}").as_str(),
+            clock.clone(),
+            spawner.clone(),
+            500,
+            500,
+            500,
+        ));
+    }
+    (ingress_pipes, ring_pipes)
+}
+
+pub fn build_limiters(
+    engine: &mut Engine,
+    config: &Config,
+    gbps: usize,
+) -> (Limiters, Limiters, Limiters) {
+    let clock = engine.default_clock();
+    let limiter_gbps = rc_limiter!(clock, gbps);
+    let top = engine.top();
+    let source_limiters: Limiters = (0..config.ring_size)
+        .map(|i| Limiter::new(top, format!("src_limit{i}").as_str(), limiter_gbps.clone()))
+        .collect();
+
+    let ring_limiters: Limiters = (0..config.ring_size)
+        .map(|i| Limiter::new(top, format!("ring_limit{i}").as_str(), limiter_gbps.clone()))
+        .collect();
+
+    let sink_limiters: Limiters = (0..config.ring_size)
+        .map(|i| Limiter::new(top, format!("sink_limit{i}").as_str(), limiter_gbps.clone()))
+        .collect();
+    (source_limiters, ring_limiters, sink_limiters)
+}
---
+++ examples/sim-ring/src/tracker_builder.rs
@@ -0,0 +1,75 @@
+// Copyright (c) 2025 Graphcore Ltd. All rights reserved.
+//
+//! Library functions to build trackers as defined by the user.
+
+use std::io::BufWriter;
+use std::sync::Arc;
+use std::{fs, io};
+
+use steam_track::tracker::{
+    CapnProtoTracker, EntityManager, TextTracker, multi_tracker::MultiTracker,
+};
+use steam_track::{Tracker, Writer};
+
+/// Create a tracker that prints to stdout
+///
+/// The user can pass a filter regular expression which will set the level only
+/// for matching Entities and set all other Entities to only emit errors.
+fn build_stdout_tracker(level: log::Level, filter_regex: &str) -> Tracker {
+    let default_level = if filter_regex.is_empty() {
+        level
+    } else {
+        log::Level::Error
+    };
+    let mut entity_manager = EntityManager::new(default_level);
+    if !filter_regex.is_empty() {
+        entity_manager.add_entity_level_filter(filter_regex, level);
+    }
+    let stdout_writer = Box::new(std::io::BufWriter::new(io::stdout()));
+    Arc::new(TextTracker::new(entity_manager, stdout_writer))
+}
+
+/// Same as the text tracker (see build_stdout_tracker) except will generate a
+/// binary file.
+fn build_binary_tracker(level: log::Level, filter_regex: &str, trace_file: &str) -> Tracker {
+    let default_level = if filter_regex.is_empty() {
+        level
+    } else {
+        log::Level::Error
+    };
+    let mut entity_manager = EntityManager::new(default_level);
+    if !filter_regex.is_empty() {
+        entity_manager.add_entity_level_filter(filter_regex, level);
+    }
+
+    let bin_writer: Writer = Box::new(BufWriter::new(fs::File::create(trace_file).unwrap()));
+    Arc::new(CapnProtoTracker::new(entity_manager, bin_writer))
+}
+
+/// Set up stdout/binary trackers according the the command-line arguments
+pub fn setup_trackers(
+    enable_stdout: bool,
+    stdout_level: log::Level,
+    stdout_filter_regex: &str,
+    enable_binary: bool,
+    binary_level: log::Level,
+    binary_filter_regex: &str,
+    binary_file: &str,
+) -> Tracker {
+    if enable_stdout && enable_binary {
+        let mut tracker = MultiTracker::default();
+
+        let log_tracker: Tracker = build_stdout_tracker(stdout_level, stdout_filter_regex);
+        let trace_tracker: Tracker =
+            build_binary_tracker(binary_level, binary_filter_regex, binary_file);
+        tracker.add_tracker(log_tracker);
+        tracker.add_tracker(trace_tracker);
+        Arc::new(tracker)
+    } else if enable_stdout {
+        build_stdout_tracker(stdout_level, stdout_filter_regex)
+    } else if enable_binary {
+        build_binary_tracker(binary_level, binary_filter_regex, binary_file)
+    } else {
+        build_stdout_tracker(log::Level::Warn, "")
+    }
+}
