585514552c5ce94f8fad9317babcdc396ee41b07
Sam Chesney <samc@graphcore.ai>
2025-08-08 17:16:10 +0100

    infra: update build dependencies

--- .github/actions/install-build-dependencies/install.sh
+++ .github/actions/install-build-dependencies/install.sh
@@ -19,8 +19,8 @@ fi
 rustup show  # Cause toolchain specified in rust-toolchain.toml to be installed
 
 cargo install --locked    \
-  cargo-expand@1.0.110    \
-  mdbook@0.4.51           \
+  cargo-expand@1.0.114    \
+  mdbook@0.4.52           \
   mdbook-cmdrun@0.7.1     \
   mdbook-keeper@0.5.0     \
   mdbook-linkcheck@0.7.7
