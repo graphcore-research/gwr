da7078e75aaaaecccf90713dae1d180a70d83c9c
Sam Chesney <samc@graphcore.ai>
2025-11-18 18:28:09 +0000

    fix(gwr-perfetto-sys,gwr-perfetto): always dirty during incremental builds
    Incremental builds were not working optimally for dependants of
    gwr-perfetto-sys, resulting in slower incremental builds.
    
    To resolve this issue, and to ensure that these packages will work as
    intended when used from crates.io,the `gwr-perfetto-sys/perfetto` symlink
    has been replaced with the `PERFETTO_SOURCE` const now published by the
    gwr-perfetto-sys library.
    
    Incremental builds now work as expected, however it is worth noting that
    if files within the gwr-perfetto-sys or gwr-perfetto packages are
    touched in between incremental builds they may continue to appear dirty
    until the next clean when running `cargo check`.

--- gwr-perfetto-sys/build.rs
+++ gwr-perfetto-sys/build.rs
@@ -1,34 +1,12 @@
 // Copyright (c) 2024 Graphcore Ltd. All rights reserved.
 
-//! Perfetto's source will be downloaded and a symlink
-//! (see the PERFETTO_SYMLINK const) created in the source tree of this package
-//! to give users of this package constant paths to tools and schema.
-//!
-//! Ideally we would have Cargo watch the PERFETTO_SYMLINK itself such that it
-//! would be recreate if deleted so that downstream builds that depend on this
-//! package do not fail unexpectedly. Unfortunately this is not possible as
-//! there appears to be no way to prevent the (mtime) check performed by Cargo
-//! following the symlink. Instead, as a compromise, we explicitly watch the
-//! PERFETTO_SYMLINK/protos directory as this is not expected to change once the
-//! Perfetto source has been downloaded (i.e. mtime check will be stable after
-//! the first rebuild attempt), and it allows us to detect if the symlink is
-//! removed.
-
 use std::env;
 use std::process::Command;
 
 pub const PERFETTO_REPO_URL: &str = "https://github.com/google/perfetto";
 pub const PERFETTO_REPO_REFSPEC: &str = "v52.0";
 
-pub const PERFETTO_SYMLINK: &str = "perfetto";
-
-fn add_file_build_triggers() {
-    println!("cargo::rerun-if-changed=src/lib.rs");
-}
-
 fn main() {
-    add_file_build_triggers();
-
     let out_dir = env::var("OUT_DIR").unwrap();
 
     let output = Command::new("git")
@@ -67,20 +45,4 @@ fn main() {
         "Failed to checkout Perfetto source repo:\n{}",
         str::from_utf8(&output.stderr).unwrap_or_default()
     );
-
-    let output = Command::new("ln")
-        .arg("-s")
-        .arg("-f")
-        .arg("-n")
-        .arg(&out_dir)
-        .arg(PERFETTO_SYMLINK)
-        .output()
-        .expect("ln command failed to start");
-    assert!(
-        output.status.success(),
-        "Failed to create symlink to Perfetto source repo:\n{}",
-        str::from_utf8(&output.stderr).unwrap_or_default()
-    );
-
-    println!("cargo::rerun-if-changed={PERFETTO_SYMLINK}/protos");
 }
--- gwr-perfetto-sys/src/lib.rs
+++ gwr-perfetto-sys/src/lib.rs
@@ -1,8 +1,5 @@
 // Copyright (c) 2024 Graphcore Ltd. All rights reserved.
 
 //! This crate is provided to wrap up the download of Perfetto.
-//!
-//! # Features
-//!
-//! Download the Perfetto source code and symlink it into the source tree of
-//! this crate.
+
+pub const PERFETTO_SOURCE: &str = env!("OUT_DIR");
