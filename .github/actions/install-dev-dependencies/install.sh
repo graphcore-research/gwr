d8ade5f229b1751c06b9e0d2b83d29b67b607a28
Sam Chesney <samc@graphcore.ai>
2026-01-27 13:41:59 +0000

    infra: update cargo-semver-checks

--- .github/actions/install-dev-dependencies/install.sh
+++ .github/actions/install-dev-dependencies/install.sh
@@ -36,6 +36,6 @@ rustup toolchain install --profile minimal --component rustfmt nightly
 cargo binstall --disable-telemetry --no-confirm --locked   \
   cargo-about@0.8.4                                        \
   cargo-deny@0.19.0                                        \
-  cargo-semver-checks@0.44.0                               \
+  cargo-semver-checks@0.46.0                               \
   release-plz@0.3.149
 cargo binstall --disable-telemetry --no-confirm --locked --bin=cog cocogitto@6.3.0
