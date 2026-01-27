81e9a57a328e01e76a06d92893e0dd2be8514c92
Sam Chesney <samc@graphcore.ai>
2026-01-27 17:06:38 +0000

    Revert "infra: update mdbook"
    This reverts commit 002742d9ed01504519712c8266e2796539d3c841.
    
    Rolling back the update to mdbook 0.5.2 due to an incompatibility with
    mdbook-runcmd 0.7.2. This issue appears to be tracked in
    https://github.com/FauconFan/mdbook-cmdrun/issues/22.
    
    This has highlighted a deficiency in the
    gwr_developer_guide::tests::mdbook_build test which is used to
    verify the `mdbook build` process is working successfully during runs
    of `cargo test`.

--- .github/actions/install-build-dependencies/install.sh
+++ .github/actions/install-build-dependencies/install.sh
@@ -69,7 +69,7 @@ curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-
 
 cargo binstall --disable-telemetry --no-confirm --locked   \
   cargo-expand@1.0.119                                     \
-  mdbook@0.5.2                                             \
+  mdbook@0.4.52                                            \
   mdbook-alerts@0.8.0                                      \
   mdbook-cmdrun@0.7.3                                      \
   mdbook-keeper@0.5.0                                      \
