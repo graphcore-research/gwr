4428bdba0943c98e2568ba5b5d8be3b6a7df2944
Sam Chesney <samc@graphcore.ai>
2026-01-27 18:33:17 +0000

    fix: explicitly check for successful exit in mdbook build test

--- gwr-developer-guide/src/lib.rs
+++ gwr-developer-guide/src/lib.rs
@@ -35,6 +35,13 @@ mod tests {
         let stdout =
             strip_str(str::from_utf8(&mdbook_output.stderr).expect("Failed to get command output"));
 
+        // Catch all signal terminations and non-zero exits
+        assert!(
+            mdbook_output.status.success(),
+            "mdbook build did not exit successfully:\n{stdout}"
+        );
+
+        // Attempt to catch warnings and errors emitted despite zero exit status
         assert!(
             !stdout.contains("(Panicked)"),
             "Error emitted during mdBook build:\n{stdout}"
