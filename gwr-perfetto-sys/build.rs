917e9edb76dba27647607f9a9625ad4f3735cbda
Sam Chesney <samc@graphcore.ai>
2026-04-01 16:26:33 +0100

    fix(gwr-perfetto-sys): don't panic when perfetto_src directory doesn't exist

--- gwr-perfetto-sys/build.rs
+++ gwr-perfetto-sys/build.rs
@@ -1,6 +1,6 @@
 // Copyright (c) 2026 Graphcore Ltd. All rights reserved.
 
-use std::fs;
+use std::fs::read_dir;
 use std::process::Command;
 
 #[path = "src/submodule_path.rs"]
@@ -22,8 +22,7 @@ fn main() {
         submodule_is_updated(),
         "{}",
         &format!(
-            "Perfetto source submodule does not appear to have been checked out.
-    Please run `git {}`.",
+            "Perfetto source submodule does not appear to have been checked out. Please run `git {}`.",
             get_git_args().join(" ")
         )
     );
@@ -39,22 +38,17 @@ fn cargo_semver_checks_enabled() -> bool {
 }
 
 fn submodule_is_updated() -> bool {
-    let mut directory_entries = fs::read_dir(submodule_path::PERFETTO_SOURCE).expect(
-        "The Perfetto source
-    directory is expected to exist whether or not the submodule has been
-    initialised and updated.",
-    );
+    let Ok(mut directory_entries) = read_dir(submodule_path::PERFETTO_SOURCE) else {
+        return false;
+    };
 
     directory_entries.next().is_some()
 }
 
 fn update_submodule() {
-    let git_command = Command::new("git")
-        .args(get_git_args())
-        .status()
-        .unwrap_or_else(|_| {
-            panic!("git should be available on the PATH when {SEMVER_ENV_VAR_KEY} is set.")
-        });
+    let Ok(git_command) = Command::new("git").args(get_git_args()).status() else {
+        panic!("git should be available on the PATH when {SEMVER_ENV_VAR_KEY} is set.")
+    };
     assert!(git_command.success(), "{}", git_command.to_string());
 }
 
