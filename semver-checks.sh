71da7deb5f2ae8374533575a62bab23f0a3f7b78
Sam Chesney <samc@graphcore.ai>
2026-04-02 12:05:00 +0100

    fix: avoid using git features of cargo-semver-checks
    Attemptin to use cargo-semver-checks with the `baseline-rev` option does
    not work with repos that include submodules. The baseline checkout that
    cargo-semver-checks creates does not include the submodule, but also
    does not include the .git directory, which prevents the fix in
    ee4b7e620dc4e268f494f882e791310816fce4e6 from working as intended.
    
    Switching to use the `baseline-root` option instead allows us to create
    the baseline checkout with the .git directory still in place.

---
+++ semver-checks.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Copyright (c) 2026 Graphcore Ltd. All rights reserved.
+
+BASELINE_DIR=target/semver-checks/baseline-repo
+REMOTE_URL=https://github.com/graphcore-research/gwr.git
+REMOTE_NAME=origin
+BRANCH=main
+
+git init "$BASELINE_DIR"
+git -C "$BASELINE_DIR" remote add "$REMOTE_NAME" "$REMOTE_URL"
+git -C "$BASELINE_DIR" fetch --depth=1 "$REMOTE_NAME" "$BRANCH"
+git -C "$BASELINE_DIR" checkout -B "$BRANCH" "$REMOTE_NAME/$BRANCH"
+
+env CARGO_SEMVER_CHECKS=1 cargo semver-checks --baseline-root "$BASELINE_DIR"
